545ff3358338892c31b6b5d5fc540c673ba7dcfa
roshankhanxox <64058135+roshankhanxox@users.noreply.github.com>
2025-01-17 21:22:37 +0530

    adding all the project files

--- trusted/eslint.config.mjs
+++ trusted/eslint.config.mjs
@@ -1,3 +1,19 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
+// export default eslintConfig;
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -6,11 +22,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.config({
+		extends: ["next"],
+		rules: {
+			"react/no-unescaped-entities": "off",
+			"@next/next/no-page-custom-font": "off",
+			"@typescript-eslint/no-unused-vars": "off",
+			"@typescript-eslint/no-explicit-any": "off",
+		},
+	}),
 ];
 
 export default eslintConfig;
